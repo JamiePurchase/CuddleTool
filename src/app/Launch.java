30e253663ce0802785bb0fe8ec50a0cbdb47fa12
Jamie Purchase <jamie.purchase@medicapp.com>
2015-11-20 21:34:16 +0000

    Added image of cmd and updated help

--- src/app/Launch.java
+++ src/app/Launch.java
@@ -18,39 +18,85 @@ public class Launch
 
     public static void main(String[] args)
     {
-        // Debug
-        System.console().writer().println("Hello 1");
-        System.console().writer().print("Hello 2");
+        // Init
+        consoleInit();
         
         // Check arguments
         if(args.length == 3)
         {
+            // Define arguments
             String path = args[0];
             String ext = args[1];
             boolean sub = false;
             if(args[2].equals("true")) {sub = true;}
             
-            /* Debug
-            ArrayList<String> arguments = new ArrayList();
-            arguments.add("Arguments");
-            arguments.add(" path = '" + path + "'");
-            arguments.add(" ext = '" + ext + "'");
-            arguments.add(" sub = '" + sub + "'");
-            */
+            // Define errors
+            ArrayList<String> errorOutput = new ArrayList();
             
-            // Check directory
-            if(new File(path).isDirectory())
+            // Check directory is valid
+            boolean pathValid = true;
+            if(!new File(path).isDirectory())
             {
-                //arguments.add(" is directory");
-                fileCuddle(path, ext, sub);
+                pathValid = false;
+                errorOutput.add("ERROR: invalid directory!");
+                errorOutput.add("");
+                errorOutput.add("You must specifiy a valid directory in which to run changes");
+                errorOutput.add("");
             }
-            //else {arguments.add(" is NOT directory");}
-            //FileSystem.saveFile(arguments, "C:/Users/Jamie/Documents/My Projects/CuddleTool/result.txt");
+            
+            // Check extension is valid
+            boolean extValid = true;
+            if(ext.length() < 1)
+            {
+                extValid = false;
+                errorOutput.add("ERROR: invalid extension!");
+                errorOutput.add("");
+                errorOutput.add("You must specifiy a valid extension for files on which to run changes");
+                errorOutput.add("");
+            }
+            
+            // Check subfolder is valid
+            boolean subValid = true;
+            if(args[2].equalsIgnoreCase("true")) {sub = true;}
+            else if(args[2].equalsIgnoreCase("false")) {sub = false;}
+            else
+            {
+                subValid = false;
+                errorOutput.add("ERROR: invalid subfolder boolean!");
+                errorOutput.add("");
+                errorOutput.add("You must specifiy 'true' or 'false' for subfolder processing");
+                errorOutput.add("");
+            }
+            
+            // Perform action
+            if(pathValid && extValid && subValid) {fileCuddle(path, ext, sub);}
+            
+            // Report errors
+            else
+            {
+                for(int e = 0; e < errorOutput.size(); e++) {System.out.println(errorOutput.get(e));}
+            }
         }
         
-        //consoleInit();
-        //String directory = "C:/Users/Jamie/Documents/My Projects/CuddleTool/";
-        //fileCuddle(directory, "txt", false);
+        // Output Help
+        else if(args[0].equalsIgnoreCase("help")) {consoleArgs();}
+        
+        // Arguments incorrect
+        else
+        {
+            System.out.println("ERROR: incorrect arguments!");
+            System.out.println("");
+            consoleArgs();
+        }
+    }
+    
+    private static void consoleArgs()
+    {
+        System.out.println("The arguments required are;");
+        System.out.println(" directory     (string) directory in which to run the tool");
+        System.out.println(" extension     (string) extension of files to change");
+        System.out.println(" subfolder     (boolean) true or false to change files in subfolders");
+        System.out.println("");
     }
     
     private static void consoleInit()
